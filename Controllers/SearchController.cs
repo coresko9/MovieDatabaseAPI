8689a4f9cf1331bb57f139a195a132d8c83d56bd
Corey Lesko <77509156+coresko9@users.noreply.github.com>
2021-04-23 18:11:16 -0400

    added random movie finder, improved layout

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -4,14 +4,13 @@ using MovieDataBase.Models;
 using Newtonsoft.Json;
 using RestSharp;
 using System;
-using System.Threading.Tasks;
 
 namespace MovieDataBase.Controllers
 {
-    
+
     public class SearchController : Controller
     {
-        
+
         private IRestResponse _resp;
         private static string _search;
         private static ViewModelMovie _data;
@@ -30,10 +29,10 @@ namespace MovieDataBase.Controllers
         [HttpPost]
         public IActionResult SearchAPI(string movieTitle)
         {
-            
+
             _data = new ViewModelMovie();
 
-            if(!string.IsNullOrWhiteSpace(movieTitle))
+            if (!string.IsNullOrWhiteSpace(movieTitle))
             {
                 movieTitle = movieTitle.Trim();
             }
@@ -41,6 +40,7 @@ namespace MovieDataBase.Controllers
             {
                 movieTitle = " ";
             }
+
             var client = new RestClient("https://movie-database-imdb-alternative.p.rapidapi.com/").AddDefaultQueryParameter("s", movieTitle);
             var request = new RestRequest(Method.GET);
             request.AddHeader("x-rapidapi-key", "08d9fc5c80mshe30902b3b9069d6p1f9f04jsnd7eaa1d2d693");
@@ -72,6 +72,38 @@ namespace MovieDataBase.Controllers
                 return View(_data);
             }
         }
+
+        
+        public IActionResult Random()
+        {
+            var randomMovie = new RandomMovie();
+            bool noResults = true;
+            Random ranNum = new Random();
+            while (noResults)
+            {
+                var client = new RestClient("https://movie-database-imdb-alternative.p.rapidapi.com/").AddDefaultQueryParameter("i", $"tt{ranNum.Next(10000001, 19916778).ToString().Substring(1)}");
+
+                var request = new RestRequest(Method.GET);
+                request.AddHeader("x-rapidapi-key", "08d9fc5c80mshe30902b3b9069d6p1f9f04jsnd7eaa1d2d693");
+                request.AddHeader("x-rapidapi-host", "movie-database-imdb-alternative.p.rapidapi.com");
+                IRestResponse response = client.Execute(request);
+
+
+                randomMovie = JsonConvert.DeserializeObject<RandomMovie>(response.Content);
+                if (randomMovie.imdbID !=null && randomMovie.Title != null)
+                {
+                    noResults = false;
+
+                }
+
+            }
+
+
+
+
+            return View("Random",randomMovie);
+
+        }
         public IActionResult AddMovie([Bind("Id,Title,PictureURL,ReleaseDate")] Movie movie)
         {
             if (ModelState.IsValid)
@@ -80,7 +112,7 @@ namespace MovieDataBase.Controllers
                 _context.SaveChanges();
             }
             _data = new ViewModelMovie();
-            
+
             var client = new RestClient("https://movie-database-imdb-alternative.p.rapidapi.com/").AddDefaultQueryParameter("s", _search);
             var request = new RestRequest(Method.GET);
             request.AddHeader("x-rapidapi-key", "08d9fc5c80mshe30902b3b9069d6p1f9f04jsnd7eaa1d2d693");
@@ -90,7 +122,7 @@ namespace MovieDataBase.Controllers
 
             this._resp = response;
             movieReturn = JsonConvert.DeserializeObject<RootObject>(this._resp.Content);
-            
+
             //new
             _data.SearchResults = movieReturn;
             _movieReturnInfo = _data.SearchResults;
@@ -101,15 +133,16 @@ namespace MovieDataBase.Controllers
 
                 ViewBag.noResults = true;
                 //changed from movieReturn
-                return View("SearchAPI",_data);
+                return View("SearchAPI", _data);
 
             }
             else
             {
+                ViewBag.OnAdded = $"Added: \"{movie.Title}\"";
                 ViewBag.SearchString = $"Results for \"{_search}\"";
                 ViewBag.noResults = false;
                 //changed from movieReturn
-                return View("SearchAPI",_data);
+                return View("SearchAPI", _data);
             }
         }
 
